77d65eaec9f76dcb019d742d8fb94308df0944f0
Jakob Lund Johannessen <jakob-lj@users.noreply.github.com>
2022-03-16 16:48:24 +0100

    chore: enable linter

--- src/test.py
+++ src/test.py
@@ -4,10 +4,7 @@ import cv2
 from imageProcessing.crosswalkDetection import detect
 
 if __name__ == '__main__':
-    detect( "assets/mock/small_crosswalk_ok_shape.png")
-
-
-
+    detect("assets/mock/small_crosswalk_ok_shape.png")
 
     cv2.waitKey(0)
 
