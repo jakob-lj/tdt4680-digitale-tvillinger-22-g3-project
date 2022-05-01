acf13d2d7bce72cc91f763178a8a0730b5a64d63
Jakob Lund Johannessen <jakob-lj@users.noreply.github.com>
2022-05-01 19:38:03 +0200

    feat: crop images

--- src/imageProcessing/imageProcess.py
+++ src/imageProcessing/imageProcess.py
@@ -3,4 +3,6 @@ import cv2
 
 
 def makeImageSmallForReport(image):
-    return cv2.resize(image, (500, 500), interpolation=cv2.INTER_AREA)
+    # return cv2.resize(image, (500, 500), interpolation=cv2.INTER_AREA)
+    cropped = image[1800:3300, 1000:2500]
+    return cv2.resize(cropped, (500, 500), interpolation=cv2.INTER_AREA)
